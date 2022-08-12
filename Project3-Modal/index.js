31298299d40a65f314089315ec6eb44d9576de5e
Ketan <ketanshetge2022@gmail.com>
2022-08-12 11:49:47 +0530

    Modal Proect

--- Project3-Modal/index.js
+++ Project3-Modal/index.js
@@ -1,12 +1,19 @@
-const LoginBtn=document.querySelector("#btn_1")
+const LoginBtn1=document.querySelector("#btn_1")
 const popUp=document.querySelector(".pop_up")
 const  cancel=document.querySelector(".cancel_btn")
+const FinalLogin=document.querySelector("#btn_2")
 
-
-LoginBtn.addEventListener("click",function(){
+LoginBtn1.addEventListener("click",function(){
     popUp.classList.add("rise");
 });
 
 cancel.addEventListener("click",function(){
     popUp.classList.remove("rise");
 })
+
+// if(input.value.length>0){
+//     FinalLogin.addEventListener("click",function(){
+//         popUp.classList.remove("rise");
+
+//     })
+// }
---
+++ Project3-Modal/readme.md
@@ -0,0 +1,3 @@
+<h1>Output</h1>
+<img src="Login.png" alt="one">
+<img src="popup.png" alt="two">
