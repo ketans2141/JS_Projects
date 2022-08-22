895702acf408584905ea1ce0be4cdcd8967f25a7
Ketan <ketanshetge2022@gmail.com>
2022-08-22 19:33:28 +0530

    Menu

---
+++ Project8-Menu/index.js
@@ -0,0 +1,6 @@
+let All=document.querySelector(".all");
+let breakfastBtn=document.querySelector(".breakfastbtn");
+let lunchBtn=document.querySelector(".lunchbtn");
+let shakesBtn=document.querySelector(".shakesbtn");
+let dinnerBtn=document.querySelector(".dinnerbtn");
+
